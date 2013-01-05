87452546cd685aebf91e9231b54701babf911612
Timothy Wood <tim@imaginesimplicity.com>
2013-01-05 00:32:04 -0500

    init code commit

---
+++ tribe-events-view-stub.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ Plugin Name: The Events Calendar: View Stub
+ Description: This plugin will help you create fully functional new views for The Events Calendar plugin that get properly integrated with the plugins settings, views, and other important bits.
+ Version: 1.0
+ Author: Modern Tribe, Inc.
+ Author URI: http://www.tri.be
+ Text Domain: 'tribe-event-view-stub'
+ License: GPLv2 or later
+*/
+
+/*
+Copyright 2009-2013 by Modern Tribe Inc and the contributors
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+/**
+ * bootstrap
+ */
+
+
+/**
+ * activation hook
+ */
+// register_activation_hook(__FILE__, 'event_event_view_stub_activate');
+// function event_event_view_stub_activate(){
+// }
