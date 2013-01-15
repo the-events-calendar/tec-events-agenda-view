83825a978eea53c70251c9367b6f1bc563baf953
Kyle Unzicker <kyle@wndfrm.com>
2013-01-15 11:05:00 -0600

    added js/css files

---
+++ inc/js/agenda-view.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function( $ ) { 
+
+	var agendaHeading = $('.tribe-events-agenda .agenda-event-heading');
+			agendaHeading.click(function(){
+				eventContent = $(this).parent('div').find('.tribe-agenda-event-description');
+				eventContent.slideToggle('fast');
+			});	
+});	
